d6738f8c32d26878b214df1bdf45d4e034452523
Amit Finzi <finzi.amit@gmail.com>
2023-06-23 00:47:12 +0300

    Adding graphics dir, place background, player and a snail

--- first_game.py
+++ first_game.py
@@ -3,47 +3,45 @@ from sys import exit
 
 screen_height = 400
 screen_width = 800
-surface_width = 100
-surface_height = 200
-w = 0
-h = 0
-w_dir = 1
-h_dir = 1
-rnd = 0
-
+ground_height = 300
+snail_x_pos = 600
+snail_step_size = 4
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width,screen_height)) # set screen size
 pygame.display.set_caption("first game") # give a title to the screen
 clock = pygame.time.Clock() # set a clock
+test_font = pygame.font.Font(None, 50)
 
-test_surface = pygame.Surface((surface_width,surface_height))
-test_surface.fill('Red') #color the surface with red color
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black')
 
+snail_surface = pygame.image.load('graphics/Snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (snail_x_pos, ground_height))
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(bottomright = (80,ground_height))
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print('collision')
 
-    screen.blit(test_surface,(w,h))
-    if w+surface_width == 800:
-        w_dir = -1
-    if w == 0:
-        w_dir = 1
-    if h+surface_height == 400:
-        h_dir = -1
-    if h == 0:
-        h_dir = 1
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface, (0,ground_height))
+    screen.blit(text_surface, (300, 50))
 
-    if w_dir == -1 and w == 1:        
-        rnd = (rnd+1) % 2
-    if rnd % 2 == 1:
-        test_surface.fill('Blue')
-    else:
-        test_surface.fill('Red')
-    w += w_dir
-    h += h_dir
+    snail_rect.x -= snail_step_size
+    if snail_rect.right <= 0: snail_rect.left = screen_width
+    screen.blit(snail_surface, snail_rect)
+
+    screen.blit(player_surface, player_rect)
+    
+
     pygame.display.update()
-    clock.tick(120) # set how much time per sec the loop will run, in this case 60.
+    clock.tick(60) # set how much time per sec the loop will run, in this case 60.
