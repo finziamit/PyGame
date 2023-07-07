55d2799cee1247366700ed1b825167c89b51f511
Amit Finzi <finzi.amit@gmail.com>
2023-07-07 16:20:25 +0300

    Initiallizing second game

---
+++ 2nd game/Juggle.py
@@ -0,0 +1,41 @@
+from pygame import *
+import sys
+
+
+def move_ball():
+    '''Set ball movement'''
+    pass
+
+
+def update_score():
+    ''' Update score for each ball touch'''
+    pass
+
+
+def move_right_hand():
+    '''Set right hand movement'''
+    pass
+
+
+def move_left_hand():
+    '''Set left hand movement'''
+    pass
+
+
+def set_components():
+    ''' Load components and set starting position '''
+    pass
+
+
+def game_play():
+    ''' Update screen by how game is going'''
+    set_components()
+    pass
+
+
+def main():
+    init()
+    game_play()
+
+if __name__ == '__main__':
+    main()
